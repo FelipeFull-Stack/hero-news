d8f534d606e0c7f6e6489779c1beaddda72f26ea
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-11-21 15:16:00 -0300

    estrutura basica simples criada

---
+++ src/components/HeroForm/index.js
@@ -0,0 +1,22 @@
+
+export function HeroForm() {
+
+    // funções
+
+
+
+    return <>
+        {/* 
+            Nome: input-nome                       -texto
+            Idade: input-idade                     -numero
+            Skills: input-skills                   -texto
+            Onde: input-onde                       -texto
+            Tempo: radios-diurno radios-noturno    -radios
+            Pagamento:                             -radios
+            Mensagem:                              -texto
+         */}
+
+
+
+    </>
+}
