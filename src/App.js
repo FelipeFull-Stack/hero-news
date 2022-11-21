4d06c9f0ba92f7aa4cf6f2bf7f90369fdd641fd9
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-11-21 18:04:34 -0300

    adicionei o axios p dar POST na API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { Routes, Route } from "react";
+import { Routes, Route } from "react-router-dom";
 import { HeroForm } from "./components/HeroForm";
 import { NavBar } from "./components/NavBar";
 import { SideBarAdds } from "./components/SideBarAdds";
-import { useState } from "react";
 
 function App() {
 
-  
+
 
 
   return (
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/" />
         <Route path="/criar-anuncio" element={<HeroForm />} />{/*criação do formulário */}
-
         <Route path="*" />
       </Routes>
       {/* <SideBarShop /> Se a gnt conseguir chegar*/}
