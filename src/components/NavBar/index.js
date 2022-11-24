ef2eb8027313be163258aba28620950d511a334c
Vitor Moraes <vitoristvan@hotmail.com>
2022-11-24 12:22:32 -0300

    nav updated

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom"
 
 export function NavBar() {
     return (
-        <nav className="bg-red-600 p-2 flex justify-between">
-            <Link to="/">Hero Adds</Link>
-            <Link to="criar-anuncio" className="mx-10 border">Criar</Link>
+        <nav className="bg-red-600 flex justify-center items-center gap-2">
+            <icon className='icon'>B</icon>
+            <icon className='icon'>o</icon>
+            <icon className='icon'>M</icon>
+            <Link to="/" className="title-nav">The Hero Journal</Link>
+            <icon className='icon'>A</icon>
+            <icon className='icon'>r</icon>
+            <icon className='icon'>!</icon>
+            {/* <Link to="criar-anuncio" className="mx-10 border">Criar</Link> */}
         </nav>
     )
 }
